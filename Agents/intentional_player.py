326998f6eb54c54213d1b262628fe99c36fc69ed
Arnav Mahajan <arnavm1499@gmail.com>
2021-03-07 01:54:43 -0500

    restructured files

---
+++ Agents/intentional_player.py
@@ -0,0 +1,119 @@
+from Agents.common_player_functions import *
+from Agents.player import *
+
+class IntentionalPlayer(Player):
+    def __init__(self, name, pnr):
+        self.name = name
+        self.hints = {}
+        self.pnr = pnr
+        self.gothint = None
+        self.last_knowledge = []
+        self.last_played = []
+        self.last_board = []
+        self.explanation = []
+    def get_action(self, nr, hands, knowledge, trash, played, board, valid_actions, hints):
+        handsize = len(knowledge[0])
+        possible = []
+        result = None
+        self.explanation = []
+        self.explanation.append(["Your Hand:"] + map(f, hands[1-nr]))
+        
+        self.gothint = None
+        for k in knowledge[nr]:
+            possible.append(get_possible(k))
+        
+        discards = []
+        duplicates = []
+        for i,p in enumerate(possible):
+            if playable(p,board):
+                result = Action(PLAY, cnr=i)
+            if discardable(p,board):
+                discards.append(i)
+
+        if discards and hints < 8 and not result:
+            result =  Action(DISCARD, cnr=random.choice(discards))
+            
+        playables = []
+        useless = []
+        discardables = []
+        othercards = trash + board
+        intentions = [None for i in xrange(handsize)]
+        for i,h in enumerate(hands):
+            if i != nr:
+                for j,(col,n) in enumerate(h):
+                    if board[col][1] + 1 == n:
+                        playables.append((i,j))
+                        intentions[j] = PLAY
+                    if board[col][1] >= n:
+                        useless.append((i,j))
+                        if not intentions[j]:
+                            intentions[j] = DISCARD
+                    if n < 5 and (col,n) not in othercards:
+                        discardables.append((i,j))
+                        if not intentions[j]:
+                            intentions[j] = CANDISCARD
+        
+        self.explanation.append(["Intentions"] + map(format_intention, intentions))
+        
+        
+            
+        if hints > 0:
+            valid = []
+            for c in ALL_COLORS:
+                action = (HINT_COLOR, c)
+                #print "HINT", COLORNAMES[c],
+                (isvalid,score,expl) = pretend(action, knowledge[1-nr], intentions, hands[1-nr], board)
+                self.explanation.append(["Prediction for: Hint Color " + COLORNAMES[c]] + map(format_intention, expl))
+                #print isvalid, score
+                if isvalid:
+                    valid.append((action,score))
+            
+            for r in xrange(5):
+                r += 1
+                action = (HINT_NUMBER, r)
+                #print "HINT", r,
+                
+                (isvalid,score, expl) = pretend(action, knowledge[1-nr], intentions, hands[1-nr], board)
+                self.explanation.append(["Prediction for: Hint Rank " + str(r)] + map(format_intention, expl))
+                #print isvalid, score
+                if isvalid:
+                    valid.append((action,score))
+                 
+            if valid and not result:
+                valid.sort(key=lambda (a,s): -s)
+                #print valid
+                (a,s) = valid[0]
+                if a[0] == HINT_COLOR:
+                    result = Action(HINT_COLOR, pnr=1-nr, col=a[1])
+                else:
+                    result = Action(HINT_NUMBER, pnr=1-nr, num=a[1])
+
+        self.explanation.append(["My Knowledge"] + map(format_knowledge, knowledge[nr]))
+        possible = [ Action(DISCARD, cnr=i) for i in xrange(handsize) ]
+        
+        scores = map(lambda p: pretend_discard(p, knowledge[nr], board, trash), possible)
+        def format_term((col,rank,n,prob,val)):
+            return COLORNAMES[col] + " " + str(rank) + " (%.2f%%): %.2f"%(prob*100, val)
+            
+        self.explanation.append(["Discard Scores"] + map(lambda (a,s,t): "\n".join(map(format_term, t)) + "\n%.2f"%(s), scores))
+        scores.sort(key=lambda (a,s,t): -s)
+        if result:
+            return result
+        return scores[0][0]
+        
+        return random.choice([Action(DISCARD, cnr=i) for i in xrange(handsize)])
+    def inform(self, action, player, game):
+        if action.type in [PLAY, DISCARD]:
+            x = str(action)
+            if (action.cnr,player) in self.hints:
+                self.hints[(action.cnr,player)] = []
+            for i in xrange(10):
+                if (action.cnr+i+1,player) in self.hints:
+                    self.hints[(action.cnr+i,player)] = self.hints[(action.cnr+i+1,player)]
+                    self.hints[(action.cnr+i+1,player)] = []
+        elif action.pnr == self.pnr:
+            self.gothint = (action,player)
+            self.last_knowledge = game.knowledge[:]
+            self.last_board = game.board[:]
+            self.last_trash = game.trash[:]
+            self.played = game.played[:]
---
+++ Agents/sampling_recognition_player.py
@@ -0,0 +1,182 @@
+from Agents.common_player_functions import *
+from Agents.player import *
+
+def do_sample(knowledge):
+    if not knowledge:
+        return []
+        
+    possible = []
+    
+    for col in ALL_COLORS:
+        for i,c in enumerate(knowledge[0][col]):
+            for j in xrange(c):
+                possible.append((col,i+1))
+    if not possible:
+        return None
+    
+    other = do_sample(knowledge[1:])
+    if other is None:
+        return None
+    sample = random.choice(possible)
+    return [sample] + other
+    
+def sample_hand(knowledge):
+    result = None
+    while result is None:
+        result = do_sample(knowledge)
+    return result
+
+class SamplingRecognitionPlayer(Player):
+    def __init__(self, name, pnr, other=IntentionalPlayer, maxtime=5000):
+        self.name = name
+        self.hints = {}
+        self.pnr = pnr
+        self.gothint = None
+        self.last_knowledge = []
+        self.last_played = []
+        self.last_board = []
+        self.other = other
+        self.maxtime = maxtime
+        self.explanation = []
+    def get_action(self, nr, hands, knowledge, trash, played, board, valid_actions, hints):
+        handsize = len(knowledge[0])
+        possible = []
+        
+        if self.gothint:
+            possiblehands = []
+            wrong = 0
+            used = {}
+            
+            for c in trash + played:
+                if c not in used:
+                    used[c] = 0
+                used[c] += 1
+            
+            i = 0
+            t0 = time.time()
+            while i < self.maxtime:
+                i += 1
+                h = sample_hand(update_knowledge(knowledge[nr], used))
+                newhands = hands[:]
+                newhands[nr] = h
+                other = self.other("Pinocchio", self.gothint[1])
+                act = other.get_action(self.gothint[1], newhands, self.last_knowledge, self.last_trash, self.last_played, self.last_board, valid_actions, hints + 1)
+                lastact = self.gothint[0]
+                if act == lastact:
+                    possiblehands.append(h)
+                    def do(c,i):
+                        newhands = hands[:]
+                        h1 = h[:]
+                        h1[i] = c
+                        newhands[nr] = h1
+                        print other.get_action(self.gothint[1], newhands, self.last_knowledge, self.last_trash, self.last_played, self.last_board, valid_actions, hints + 1)
+                    #import pdb
+                    #pdb.set_trace()
+                else:
+                    wrong += 1
+            #print "sampled", i
+            #print len(possiblehands), "would have led to", self.gothint[0], "and not:", wrong
+            #print f(possiblehands)
+            if possiblehands:
+                mostlikely = [(0,0) for i in xrange(len(possiblehands[0]))]
+                for i in xrange(len(possiblehands[0])):
+                    counts = {}
+                    for h in possiblehands:
+                        if h[i] not in counts:
+                            counts[h[i]] = 0
+                        counts[h[i]] += 1
+                    for c in counts:
+                        if counts[c] > mostlikely[i][1]:
+                            mostlikely[i] = (c,counts[c])
+                #print "most likely:", mostlikely
+                m = max(mostlikely, key=lambda (card,cnt): cnt)
+                second = mostlikely[:]
+                second.remove(m)
+                m2 = max(second, key=lambda (card,cnt): cnt)
+                if m[1] >= m2[1]*a:
+                    #print ">>>>>>> deduced!", f(m[0]), m[1],"vs", f(m2[0]), m2[1]
+                    knowledge = copy.deepcopy(knowledge)
+                    knowledge[nr][mostlikely.index(m)] = iscard(m[0])
+
+        
+        self.gothint = None
+        for k in knowledge[nr]:
+            possible.append(get_possible(k))
+        
+        discards = []
+        duplicates = []
+        for i,p in enumerate(possible):
+            if playable(p,board):
+                return Action(PLAY, cnr=i)
+            if discardable(p,board):
+                discards.append(i)
+
+        if discards:
+            return Action(DISCARD, cnr=random.choice(discards))
+            
+        playables = []
+        for i,h in enumerate(hands):
+            if i != nr:
+                for j,(col,n) in enumerate(h):
+                    if board[col][1] + 1 == n:
+                        playables.append((i,j))
+        playables.sort(key=lambda (i,j): -hands[i][j][1])
+        while playables and hints > 0:
+            i,j = playables[0]
+            knows_rank = True
+            real_color = hands[i][j][0]
+            real_rank = hands[i][j][0]
+            k = knowledge[i][j]
+            
+            hinttype = [HINT_COLOR, HINT_NUMBER]
+            if (j,i) not in self.hints:
+                self.hints[(j,i)] = []
+            
+            for h in self.hints[(j,i)]:
+                hinttype.remove(h)
+            
+            if HINT_NUMBER in hinttype:
+                self.hints[(j,i)].append(HINT_NUMBER)
+                return Action(HINT_NUMBER, pnr=i, num=hands[i][j][1])
+            if HINT_COLOR in hinttype:
+                self.hints[(j,i)].append(HINT_COLOR)
+                return Action(HINT_COLOR, pnr=i, col=hands[i][j][0])
+            
+            playables = playables[1:]
+        
+        for i, k in enumerate(knowledge):
+            if i == nr:
+                continue
+            cards = range(len(k))
+            random.shuffle(cards)
+            c = cards[0]
+            (col,num) = hands[i][c]            
+            hinttype = [HINT_COLOR, HINT_NUMBER]
+            if (c,i) not in self.hints:
+                self.hints[(c,i)] = []
+            for h in self.hints[(c,i)]:
+                hinttype.remove(h)
+            if hinttype and hints > 0:
+                if random.choice(hinttype) == HINT_COLOR:
+                    self.hints[(c,i)].append(HINT_COLOR)
+                    return Action(HINT_COLOR, pnr=i, col=col)
+                else:
+                    self.hints[(c,i)].append(HINT_NUMBER)
+                    return Action(HINT_NUMBER, pnr=i, num=num)
+
+        return random.choice([Action(DISCARD, cnr=i) for i in xrange(handsize)])
+    def inform(self, action, player, game):
+        if action.type in [PLAY, DISCARD]:
+            x = str(action)
+            if (action.cnr,player) in self.hints:
+                self.hints[(action.cnr,player)] = []
+            for i in xrange(10):
+                if (action.cnr+i+1,player) in self.hints:
+                    self.hints[(action.cnr+i,player)] = self.hints[(action.cnr+i+1,player)]
+                    self.hints[(action.cnr+i+1,player)] = []
+        elif action.pnr == self.pnr:
+            self.gothint = (action,player)
+            self.last_knowledge = game.knowledge[:]
+            self.last_board = game.board[:]
+            self.last_trash = game.trash[:]
+            self.played = game.played[:]
