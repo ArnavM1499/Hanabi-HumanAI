cc188f68f0989f0cbd05bb163c7f4dc342c5d97e
Arnav Mahajan <arnavm1499@gmail.com>
2021-04-11 18:20:20 -0400

    created chief player file

---
+++ Agents/ChiefAgent/chief_player.py
@@ -0,0 +1,15 @@
+from common_game_functions import *
+from Agents.common_player_functions import *
+from Agents.player import Player, Action
+from Agents.ChiefAgent.player_pool import PlayerPool
+
+class ChiefPlayer(Player):
+	def __init__(self, name, pnr):
+		self.name = name
+		self.pnr = pnr
+
+	def get_action(self, game_state, player_model):
+		pass
+
+	def inform(self, action, player, game_state, player_model):
+		pass
