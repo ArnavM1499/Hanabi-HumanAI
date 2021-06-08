165cfb553416ac42cca2124d70c7243cc4e70e7b
Arnav Mahajan <arnav@andrew.cmu.edu>
2021-06-08 01:10:58 -0400

    finished experiment file, untested

--- card_info_experiment.py
+++ card_info_experiment.py
@@ -20,21 +20,36 @@ for i in range(100):
 pickle_file.close()
 
 CardDrawnMove = dict()
+CardPrevKnowledge = dict()
 PosToCard = dict()
 CardToPos = dict()
 new_id = 0
 
+PlayDiscardDistances = []
+HintDistances = []
+
+def unequal(X, Y):
+	return X != Y
+
 with open(pickle_file_name, 'rb') as f:
 	row = try_pickle(f)
 
+	move_num = 0
+
 	while(row != None):
-		if row[0] == "Action":
+		print(move_num, len(PlayDiscardDistances), len(HintDistances))
+
+		if row[0] == "Action" and row[1].get_current_player() == 0:
+			move_num += 1
+
 			game_state = row[1]
 			player_model = row[2]
 			action = row[3]
 
+			# Shift positions accordingly if a card was played or discarded
 			if action.type in [hanabi.PLAY, hanabi.DISCARD]:
 				newPosToCard = dict()
+				PlayDiscardDistances.append(move_num - CardDrawnMove[PosToCard[action.cnr]])
 
 				for idx in PosToCard:
 					if idx == action.cnr:
@@ -45,9 +60,6 @@ with open(pickle_file_name, 'rb') as f:
 						CardToPos[PosToCard[idx]] -= 1
 						PosToCard[idx - 1] = PosToCard[idx]
 						del PosToCard[idx]
-				
-
-
 
 			# Add all unmapped cards to the three dictionaries
 			for idx, hand in enumerate(player_model.get_knowledge()):
@@ -59,15 +71,25 @@ with open(pickle_file_name, 'rb') as f:
 
 				PosToCard[idx] = card_id
 				CardToPos[card_id] = idx
+				CardDrawnMove[card_id] = move_num
+				CardPrevKnowledge[card_id] = player_model.get_knowledge()[idx]
 
-
-			# For mapped cards, update Position dicts if needed
-
-
-
-			# For most recent action regarding our own cards, add distance from drawn move
+			# For most recent update to info
+			for idx in PosToCard:
+				if (unequal(CardPrevKnowledge[PosToCard[idx]], player_model.get_knowledge()[idx])):
+					HintDistances.append(move_num - CardDrawnMove[PosToCard[idx]])
+					CardPrevKnowledge[PosToCard[idx]] = player_model.get_knowledge()[idx]
 
 			
 
 		row = try_pickle(f)
 
+
+def summarize_list(L, name):
+	print(name, "---------------------------------------")
+	print("        # of times recorded:", len(L))
+	print("        mean:", np.mean(L))
+	print("        variance:", np.var(L))
+
+summarize_list(PlayDiscardDistances, "Moves before play/discard")
+summarize_list(HintDistances, "Moves from draw till hint")
